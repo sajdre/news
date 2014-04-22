9b0c19ed90b8c94ca79d84bdf5dc45847e7e6826
Saje <sajdre@gmail.com>
2014-04-22 20:27:37 +0400

    fix base, nullpointer on beanService,

--- service/src/main/java/com/pvt/NewsService.java
+++ service/src/main/java/com/pvt/NewsService.java
@@ -14,6 +14,7 @@ import java.util.List;
  */
 public class NewsService {
     NewsDao newsdao = new NewsDao();
+    int i = 1;
     public void saveNews(News news){
         newsdao.save(news);
     }
